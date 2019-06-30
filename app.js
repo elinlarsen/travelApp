6e01638632f7d70eed3b641ee0f8efc0d745052e
yacinebe <yacineberrada@gmail.com>
2019-06-30 15:45:51 +0200

    fixed css imports to display styles

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 require("./config/db_connection");
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
 const app = express();
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-const mongoose=require("mongoose")
-const logger       = require('morgan');
-const path         = require('path');
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
-
-
 
 // Middleware Setup
 app.use(logger("dev"));
@@ -27,21 +25,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 //Registering partials
 hbs.registerPartials(__dirname + "/views/partials");
 
-
-//session 
+//session
 /*
 app.use(session({
   secret: "basic-auth-secret",
@@ -54,11 +53,10 @@ app.use(session({
 */
 
 // default value for title local
-app.locals.title = 'Travel App';
+app.locals.title = "Travel App";
 app.locals.site_URL = process.env.SITE_URL;
 
-const index = require('./routes/index');
-app.use('/', index);
-
+const index = require("./routes/index");
+app.use("/", index);
 
 module.exports = app;
