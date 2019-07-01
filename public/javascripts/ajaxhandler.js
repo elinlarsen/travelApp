c8b043bfc7452415ed5b2aba47abbce455b555af
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-01 22:24:45 +0200

    fixing ajax handler

--- public/javascripts/ajaxhandler.js
+++ public/javascripts/ajaxhandler.js
@@ -12,40 +12,45 @@ class ajaxHandler {
     this.instance
       .get(this.endpoint)
       .then(serverRes => clbk(serverRes.data))
-      .catch(serverErr => console.log("No data retrieved"));
+      .catch(serverErr => console.log("No data retrieved--------", serverErr));
   }
 
   getOne(id, clbk) {
-    console.log("contacting endpoint " + this.endpoint + "/" + id);
-    this.instance.get(this.endpoint + "/" + id).then(serverRes => {
-      clbk(serverRes.data);
-    });
+    console.log(`contacting endpoint ${this.endpoint}/${id}`);
+    this.instance.get(`${this.endpoint}/${id}`)
+      .then(serverRes => {
+          clbk(serverRes.data);
+      })
+      .catch(serverErr => console.log("ERROR WHILE GETTING ONE DATA : ", serverErr))    
   }
-  //.catch(serverErr => console.log("No data retrieved"));
+
 
   createOne(data, clbk) {
+    let url=`${this.endpoint}/${data}`
+    console.log("URL CREATED -----------", url)
     this.instance
-      .post(this.endpoint, data)
+      .post(this.endpoint,data)
       .then(serverRes => {
         clbk(serverRes.data);
-        console.log(serverRes.data);
+        console.log("serverRes.data createOne ------" ,serverRes.data);
       })
-      .catch(serverErr => console.log("no data retrieved"));
+      .catch(serverErr => console.log("no data created", serverErr));
   }
 
   deleteOne(id, clbk1, clbk2) {
+    let url=`${this.endpoint}/${id}`
     this.instance
-      .delete(this.endpoint + id)
+      .delete(`${this.endpoint}/${id}`)
       .then(serverRes => clbk1(serverRes))
       .catch(serverErr => {
-        console.log("No data deleted");
-        clbk2(serverErr);
+        console.log("No data deleted --------", serverErr);
+        //clbk2();
       });
   }
 
   updateOne(id, data, clbk) {
     this.instance
-      .patch(this.endpoint + id, data)
+      .patch(`${this.endpoint}/${id}`, data)
       .then(serverRes => clbk(serverRes.data))
       .catch(serverErr => console.log("No data patched"));
   }
