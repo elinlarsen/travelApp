2e3acf40f32cdce199a305e01c373ea587ec2450
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-02 12:55:19 +0200

    fix month,  starting at 0

--- utils/changeDateFormat.js
+++ utils/changeDateFormat.js
@@ -1,7 +1,8 @@
 function changeDateFormat(dateMongo){
     let date= new Date(dateMongo)
     let day=date.getDate() <10 ? [0, date.getDate()].join("") : date.getDate()
-    let month=date.getMonth()+1 <10 ? [0, date.getMonth()].join("") : date.getMonth()
+    let month=date.getMonth()+1
+    month=month <10 ? [0, month].join("") : month
     let year=date.getFullYear()
     return `${day}/${month}/${year}`
   
