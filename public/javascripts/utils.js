f040101e9ab386325ac1971dba13fca3d6a66e6b
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-02 17:01:06 +0200

    fixed utils

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -8,11 +8,12 @@ function extractTrip(tripId) {
   return tripData;
 }
 
-function changeDateFormat(dateMongo) {
-  let date = new Date(dateMongo);
-  let day = date.getDate() < 10 ? [0, date.getDate()].join("") : date.getDate();
-  let month =
-    date.getMonth() + 1 < 10 ? [0, date.getMonth()].join("") : date.getMonth();
-  let year = date.getFullYear();
-  return `${month}/${day}/${year}`;
+function changeDateFormat(dateMongo){
+  let date= new Date(dateMongo)
+  let day=date.getDate() <10 ? [0, date.getDate()].join("") : date.getDate()
+  let month=date.getMonth()+1
+  month=month <10 ? [0, month].join("") : month
+  let year=date.getFullYear()
+  return `${month}/${day}/${year}`
+
 }
