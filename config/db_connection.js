5335cae6791b02aa253b0bf766352f8345460a4d
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-05 14:28:18 +0200

    db config name

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,6 +1,6 @@
 const mongoose     = require('mongoose');
 mongoose
-  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
